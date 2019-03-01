8a82a36b38b0424261a0859204bc5f1663221077
Gary <gary@kodal.uk>
2019-03-01 00:58:50 +0000

    Begin Rewrite

--- spider.py
+++ spider.py
@@ -1,127 +1,124 @@
-
-
-import urllib
-from urllib.request import urlopen
-from urllib.parse import urlparse
 import time
 from queue import *
 import threading
 from worker import Worker
-#from conn import *
-#import pymysql.cursors
-import pymysql
-import sqlalchemy
-from sqlalchemy import create_engine
+import MySQLdb
 
 
-class spider():
-
+class Spider:
 
     def __init__(self):
 
-        #db connect
-        #self.db_engine = create_engine('mysql+pymysql://gary:Sarah2004!@212.67.214.24/0090-scraping', max_overflow=10, pool_size=5, pool_recycle=3600)
-        self.db_engine = create_engine('mysql+pymysql://gary:Sarah2004!@212.67.214.24/0090-scraping')
+        self.pending = Queue()
+        self.db_engine = MySQLdb.connect(
+            host='127.0.0.1',
+            user='root',
+            passwd='root',
+            database='pySpider'
+        )
 
         # set vars 
         self.allExtLinks = Queue()
-        self.maxThreads = 5
+        self.maxThreads = 3
         self.workers = []
-        self.running  = True
+        self.running = True
         self.horizon = []
         self.lock = threading.Semaphore(1)
 
-        #initial Links
-        self.loadHorizon()
+        # initial Links
+        self.loadhorizon()
         if int(self.allExtLinks.qsize()) < 1:
-            self.startLink()
+            self.startlink()
 
-        #run the spider
+        # run the spider
         self.run()
 
-
-    def startLink(self):
-        '''
+    def startlink(self):
+        """
         add the start link to the queue
-        '''
+        """
         self.allExtLinks.put({ 
             'url': 'http://www.reddit.com/'
         })
 
-
-    def createWorker(self, allExtLinks, theadNum, lock):
-        '''
+    def createworker(self, allExtLinks, theadNum, lock):
+        """
         create the workers
-        '''
+        :param allExtLinks:
+        :param theadNum:
+        :param lock:
+        :return:
+        """
         return Worker(allExtLinks, theadNum, lock, self.db_engine)
 
-
-    def loadHorizon(self):
-        '''
-        load the horizon list - links not yet visited 
-        '''
-        print(' ---  Begin load pending ---')
-
-        for row in self.db_engine.execute('select url from pending'):
+    def loadhorizon(self):
+        """
+        load the horizon list - links not yet visited
+        """
+        cursor = self.db_engine.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select url from pending  order by id asc limit 100')
+        rows = cursor.fetchall()
+        for row in rows:
             self.allExtLinks.put({ 
                 'url': row['url']
             })
 
-        print(' ---  End load pending ---')
-
-
     def run(self):
-        '''
+        """
         run the app
-        '''
+        """
+        # we have 1 active link
+        activethreads = 1
 
-        #we have 1 active link
-        activeThreads = 1
+        # the sql worker
 
-        #the sql worker
-        self.pending  = Queue()
+        # while we are running
+        while self.running:
 
-        #while we are running
-        while self.running :
-
-            #show the the loop is running
+            # show the the loop is running
+            '''
             print(' ')
-            print(' -------- Horizon ' + str(self.allExtLinks.qsize()) + ', Threads: ' + str(threading.activeCount()) +' ----------'  )
+            print(' -------- Horizon {0}, Threads: {1} ----------'.format(str(self.allExtLinks.qsize()),
+                                                                       str(threading.activeCount())))
             print(' ')
+            '''
 
-            #if thread count < max - start new thread
+            # if thread count < max - start new thread
             if threading.activeCount() < self.maxThreads:
 
-                w = self.createWorker( self.allExtLinks, activeThreads, self.lock )
-                activeThreads = activeThreads + 1
+                w = self.createworker(self.allExtLinks, activethreads, self.lock)
+                activethreads = activethreads + 1
                 self.workers.append(w)
                 w.start()
                     
-            #end the dead workers
+            # end the dead workers
             for w in self.workers:
 
                 # join the dead threads and count
-                if( w.isAlive() == False):
+                if not w.isAlive():
                     w.join()
-                    activeThreads = activeThreads - 1
+                    activethreads = activethreads - 1
                     self.workers.remove(w)
 
-            #sleep 1 second per loop
-            time.sleep(1)    
+            # # sleep per loop
+            # time.sleep(0.25)
 
-            #end the loop if no more
-            if( self.allExtLinks.empty() == True ):
+            if self.allExtLinks.empty():
                 self.running = False
 
-        #join active threads - to end the app
-        while threading.activeCount()>1:            
+        # join active threads - to end the app
+        while threading.activeCount() > 1:
             for w in self.workers:
                 w.join()             
 
-        ## waiting for output
-        print(' -------- Complete: horizon ' + str(self.allExtLinks.qsize()) + ', Threads: ' + str(threading.activeCount()) + ', saved: ' + str(self.written) +' ----------'  )
+        # waiting for output
+        '''        
+        print(
+            ' -------- Complete: horizon {0}, Threads: {1}, saved: {2} ----------'.format(str(self.allExtLinks.qsize()),
+                                                                                          str(threading.activeCount()),
+                                                                                          str(self.written)))
+        '''
+
 
 if __name__ == '__main__':
-    s = spider();
-
-    
+    s = Spider()
--- worker.py
+++ worker.py
@@ -1,5 +1,3 @@
-
-
 import sys, traceback
 from threading import Thread
 import urllib
@@ -7,77 +5,70 @@ from urllib.request import urlopen
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import logging
-import re
 from queue import *
 import time
-import pymysql
-import sqlalchemy
-from sqlalchemy import create_engine
-
+import MySQLdb
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 class Worker(Thread):
+    running: bool
 
-
-    def __init__( self, queue, threadNum, lock, engine ):
-        '''
+    def __init__(self, queue, threadNum, lock, engine):
+        """
         init with the queue and set the logger - this worker is a thread extended class
-        '''
+        """
         Thread.__init__(self)
-        self.threadNum = str(threadNum)    
+        self.threadNum = str(threadNum)
         self.queue = queue;
         self.running = True
         self.lock = lock
         self.excludedDomains = []
-        self.setExcludedDomains()
-        self.engine = engine
+        self.setexcludedeomains()
+        self.db_engine = engine
 
-
-    def setExcludedDomains(self):
-        '''
+    def setexcludedeomains(self):
+        """
         domains that we want to exclude from the search
-        '''
+        """
         self.excludedDomains.append('www.reddit.com')
         self.excludedDomains.append('www.deviantart.com')
         self.excludedDomains.append('comments.deviantart.com')
 
-
     def join(self, timeout=None):
-        '''
+        """
         when the thread joins send the loop end signal
-        '''
+        """
         self.running = False
-        super(Worker, self).join(timeout)      
+        super(Worker, self).join(timeout)
 
-
-    def getCurrentDomain(self, page):
-        '''
+    @staticmethod
+    def getcurrentdomain(page):
+        """
         get the domain we are crawling
-        '''
+        """
         parsed_uri = urlparse(page)
         return parsed_uri.netloc
 
-
-    def encodeLink(self, link):
-        '''
+    @staticmethod
+    def encodelink(link):
+        """
         attempt to fix encoding issues with links
-        '''
+        """
         input = bytes(link, "UTF-8")
         link = input.decode("ascii", "ignore")
         return link;
 
-
     def fetch(self, uri):
-        '''
+        """
         url open, check the headers for text/html
         if so return data
-        '''
-        uri = self.encodeLink(uri)      
+        """
+        uri = self.encodelink(uri)
         try:
             h = urlopen(uri)
-            x =  h.info()
+            x = h.info()
             if x['Content-Type'] != None and 'text/html' in x['Content-Type'].lower():
                 return h.read()
             else:
@@ -85,31 +76,30 @@ class Worker(Thread):
         except urllib.error.URLError:
             return None
 
-
-    def getLinks(self, page, url):
-        '''
+    def getlinks(self, page, url):
+        """
         find all anchor linsk within the page
         add to either array depending on its http(s) status
-        '''
+        """
         internalLinks = [];
         externalLinks = [];
-        currentDomain = self.getCurrentDomain(url)
+        currentDomain = self.getcurrentdomain(url)
         self.urlparse = urlparse(url)
 
         for link in page.findAll('a', href=True):
 
             if link.has_attr('href'):
-                
-                if( link['href'].startswith('#') == False and link['href'] != '/' ):
-     
-                    #internal link - rel link
-                    if currentDomain not in link['href'] and link['href'].startswith('http://') == False and link['href'].startswith('https://') == False :
 
+                if (link['href'].startswith('#') == False and link['href'] != '/'):
 
-                        if currentDomain.endswith('/') :
+                    # internal link - rel link
+                    if currentDomain not in link['href'] and link['href'].startswith('http://') == False and link[
+                        'href'].startswith('https://') == False:
+
+                        if currentDomain.endswith('/'):
                             currentDomain = currentDomain[:-1]
 
-                        if link['href'].startswith('/') :
+                        if link['href'].startswith('/'):
                             link['href'] = link['href'][1:]
 
                         link['href'] = self.urlparse.scheme + '://' + currentDomain + '/' + link['href']
@@ -118,117 +108,116 @@ class Worker(Thread):
 
                         internalLinks.append(link)
 
-
                     # external link
-                    elif currentDomain not in link['href'] and ( link['href'].startswith('http://') or link['href'].startswith('https://') ) :
-
+                    elif currentDomain not in link['href'] and (
+                            link['href'].startswith('http://') or link['href'].startswith('https://')):
                         link = link['href']
-
                         externalLinks.append(link)
 
-
                     # internal link non rel
-                    elif currentDomain in link['href'] :
+                    elif currentDomain in link['href']:
 
                         if link['href'].startswith('http://') == False and link['href'].startswith('https://') == False:
-                        
-                            if currentDomain.endswith('/') :
+                            if currentDomain.endswith('/'):
                                 currentDomain = currentDomain[:-1]
 
-                            if link['href'].startswith('/') :
+                            if link['href'].startswith('/'):
                                 link['href'] = link['href'][1:]
 
                             link['href'] = self.urlparse.scheme + '://' + currentDomain + '/' + link['href']
-
                         link = link['href']
-
                         internalLinks.append(link)
-
         return internalLinks, externalLinks
 
-
-    def inQueue(self, url):
-        '''
+    def inqueue(self, url):
+        """
         test to see if the url is part of the pending list
-        '''
-        #not in pending list
-        result = False   
-        connection = self.engine.connect()
-        result = connection.execute( "select count(*) as total from pending where `url`=%s", (url) )
-        for row in result:
-            if( row['total']) > 0:
+        """
+        result = False
+        cursor = self.db_engine.cursor(MySQLdb.cursors.DictCursor)
+        sql = " select count(*) as total from pending where `url`= '" + url + "' "
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        for row in rows:
+            if row['total'] > 0:
                 result = True
-        connection.close()
+        cursor.close()
         return result
 
-
-    def writeLinksToPending(self, url):
-        '''
+    def writelinkstopending(self, url):
+        """
         add the link to the pending table
-        '''
-        connection = self.engine.connect()
-        connection.execute( "insert into pending (url, dateCreated, dateUpdated) values ( %s, NOW(), NOW() )" , (url) )
-        connection.close()
+        """
+        sql = "insert into pending (url, dateCreated, dateUpdated) values ( '" + url + "', NOW(), NOW() )"
+        try:
+            cursor = self.db_engine.cursor(MySQLdb.cursors.DictCursor)
 
+            cursor.execute(sql)
+            self.db_engine.commit()
+            cursor.close()
+        except:
+            print('===> error(sql): ' + sql)
 
     def run(self):
-        '''
+        """
         thread run, check url
-        '''
-        #get the item from the queue
+        """
+        # get the item from the queue
         item = self.queue.get()
-        
-        #while there is an item in the current queue
-        while self.running == True:                 
+
+        # while there is an item in the current queue
+        while self.running:
 
             try:
 
-                #the current url
+                # the current url
                 url = item['url']
-                currentDomain = self.getCurrentDomain(url)
+                currentDomain = self.getcurrentdomain(url)
 
-                #fetch the html
+                # fetch the html
                 data = self.fetch(url)
 
-                #only if we have a page
+                # only if we have a page
                 if data != None:
 
-                    #log the current url we are scraping
-                    #logging.info('[+] Thread: ' + self.threadNum + ' - Success fetched: %s', url)
+                    # log the current url we are scraping
+                    # logging.info('[+] Thread: ' + self.threadNum + ' - Success fetched: %s', url)
                     print('[+] Thread: ' + self.threadNum + ' - Success fetched: %s', url);
 
-                    #create the beautifulSoup object
+                    # create the beautifulSoup object
                     bsObj = BeautifulSoup(data, 'lxml')
 
-                    #get the internal and external links
-                    internalLinks, externalLinks = self.getLinks(bsObj, url)
-                    
-                    # only scrape pages that are relative to the start page
-                    for i in internalLinks:
-                        #if self.inQueue(i) == False:
-                        #self.writeLinksToPending(i)
-                        self.queue.put({ 'url' : i })
+                    # get the internal and external links
+                    internalLinks, externalLinks = self.getlinks(bsObj, url)
 
-                    #add to the queue of external links
+                    # only scrape pages that are relative to the start page
+                    '''
+                    for i in internalLinks:
+                        if not self.inqueue(i):
+                            self.writelinkstopending(i)
+                            # self.queue.put({'url': i})
+                    '''
+
+                    # add to the queue of external links
                     for i in externalLinks:
-                        #if self.inQueue(i) == False:
-                        #self.writeLinksToPending(i)
-                        self.queue.put({ 'url' : i })
-                    
+                        if not self.inqueue(i):
+                            self.writelinkstopending(i)
+                            # self.queue.put({'url': i})
+
                     time.sleep(1)
 
-                #set the next item for the while loop
-                item = self.queue.get()                
+                # set the next item for the while loop
+                item = self.queue.get()
 
             except Empty as e:
                 print(' ')
                 print(' -------- Thread empty ' + self.threadNum + ' url: ' + url + ' -------- ');
                 print(e)
                 print(' ')
-                
-                print("-"*60)
+
+                print("- " * 60)
                 traceback.print_exc(file=sys.stdout)
-                print("-"*60)
+                print("- " * 60)
 
                 self.running = False
 
@@ -238,10 +227,10 @@ class Worker(Thread):
                 print(e)
                 print(' ')
 
-                print("-"*60)
+                print("- " * 60)
                 traceback.print_exc(file=sys.stdout)
-                print("-"*60)
+                print("- " * 60)
 
                 continue
 
-        self.queue.task_done()  
+        self.queue.task_done()
